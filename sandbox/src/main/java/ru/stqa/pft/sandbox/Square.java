f0134e7140f62a0a190c0f5683644031164af489
proluiza <proluiza@mail.ru>
2017-09-17 20:50:15 +0300

    ДЗ

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l) {
+        this.l = 1;
+    }
+
+    public double area(){
+        return  this.l * this.l;
+    }
+}
