50c7d31addcd6bf95763df0c80c9341858651fc8
proluiza <proluiza@mail.ru>
2017-09-19 17:58:18 +0300

    l2_m5

--- sandbox/src/test/java/ru/stqa/pft/sanbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sanbox/SquareTests.java
@@ -4,12 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.sandbox.Square;
 
+
+
+
 public class SquareTests {
 
     @Test
     public void testArea() {
         Square s = new Square(5);
         Assert.assertEquals(s.area(),25.0);
+
     }
 
 }
