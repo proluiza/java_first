1ac7ee4ac423be8f799aba4100217c5352b66eab
proluiza <proluiza@mail.ru>
2017-11-08 12:16:32 +0300

    рефакторинг

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CroupCreationTest.java
@@ -34,7 +34,7 @@ public class CroupCreationTest {
     public void testCroupCreationTest() {
         goToGroupPage();
         initGroupCreation();
-        fillGroupForm("Ruru2", "Ruru3", "Ruru4");
+        fillGroupForm(new GroupDate("Ruru2", "Ruru3", "Ruru4"));
         submitGroupCreation();
         returnToGroupPage();
     }
@@ -47,16 +47,16 @@ public class CroupCreationTest {
         wd.findElement(By.name("submit")).click();
     }
 
-    private void fillGroupForm(String name, String header, String footer) {
+    private void fillGroupForm(GroupDate groupDate) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(name);
+        wd.findElement(By.name("group_name")).sendKeys(groupDate.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(header);
+        wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(footer);
+        wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
     }
 
     private void initGroupCreation() {
